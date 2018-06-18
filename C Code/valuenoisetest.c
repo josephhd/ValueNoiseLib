fac7e068c689377be28747e0399fbba65628ce4f
josephhd <jddevstudio@gmail.com>
2018-06-18 12:21:24 -0600

    Add files via upload

--- C Code/valuenoisetest.c
+++ C Code/valuenoisetest.c
@@ -2,19 +2,22 @@
 #include "valuenoise.h"
 #include "vectors.h"
 
-#define width 128
+#define width 256
+#define height 256
 
 int main () {
     struct double3 value;
     
     FILE* fp = fopen ("valuenoise.pgm", "w");
-    fprintf (fp, "P2\n%d %d\n255\n", width, width);
+    fprintf (fp, "P2\n%d %d\n255\n", width, height);
 
-    for (int y = 0; y < width; y++) {
+    for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
-            value = ValueNoise2D (x, y, 0.1, 5, 2, 0.5);
+            value = ValueNoise2D (x, y, 0.05, 5, 2, 0.5);
             fprintf (fp, "%u ", (unsigned)(value.x * 255));
         }
+
+        fprintf (fp, "\n");
     }
 
     fclose (fp);
